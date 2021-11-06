3f1c79ef234931c638a7d66d321d272dcf591f19
Luis <luisgoncalvesfc@gmail.com>
2021-11-06 15:31:20 +0000

    factorial functions

---
+++ factorial.js
@@ -0,0 +1,17 @@
+// n! -> k = n * (n-1) * (n-2) * (n-3) ... 
+
+// iteration function to factorial number
+const factorial_iter = (n) =>{
+  let a = 1;
+  for (let i = 1;i <= n;i++){
+    a *= i;
+  }
+  return a;
+};
+
+
+// recursive function to factorial number
+const factorial_recur = (n) => {
+  if (n == 0) return 1;
+  return n * factorial_recur(n - 1);
+};
--- recursion.js
+++ recursion.js
@@ -1,25 +1,6 @@
 // Iteration and recursion are key Computer Science techniques used in creating algorithms and developing software.
 // Iterative function is one that loops to repeat some part of the code, and a recursive function is one that calls itself again to repeat the code.
 
-// factorial algorithm
-    // n! -> k = n * (n-1) * (n-2) * (n-3) ... 
-
-// iteration function to factorial number
-const factorial_iter = (n) =>{
-  let a = 1;
-  for (let i = 1;i <= n; i++){
-    a *= i;
-  }
-  return a;
-};
-
-
-// recursive function to factorial number
-const factorial_recur = (n) => {
-  if (n == 0) return 1;
-  return n*factorial_recur(n-1);
-};
-
 
 // for loop 3,21ms runtime
 const print_values = array => {
