53f2b51de5d44f74188ff3b5b2ae758e06a7557d
Luis <luisgoncalvesfc@gmail.com>
2021-11-07 19:00:22 +0000

    mutability

---
+++ mutability.js
@@ -0,0 +1,23 @@
+const User = {
+  name: "Tiago",
+  location: "Lagos"
+};
+
+console.log(User);
+
+// allows mutation of the state
+const mutableUser = User;
+mutableUser.location = "Lisboa";
+mutableUser.name = "Miguel";
+console.log(mutableUser);
+
+//immutable object
+const newUser = (name, location) => {
+  return {
+    name: name,
+    location: location,
+  }
+}
+
+const immutableUser = Object.freeze(newUser("Luis", "Porto"));
+console.log(immutableUser);
