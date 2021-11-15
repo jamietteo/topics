97f9c4a298000084dfb4f8857b6d7a10bf487d89
Rastrinak <luisgoncalvesfc@gmail.com>
2021-11-15 15:00:56 +0000

    better identation

--- types.ts
+++ types.ts
@@ -1,13 +1,29 @@
 // Primitive types
 
-type Nullish = null | undefined;
-type Names = 'luis' | 'miguel' | 'jorge';
-type Num = number | bigint;
+type Nullish =
+  | null
+  | undefined;
+
+type Names =
+  | 'luis'
+  | 'miguel'
+  | 'jorge';
+
+type Num =
+  | number
+  | bigint;
 
 // Union types
 
-type Fruit = 'apple' | 'pear' | 'orange';
-type Vegetable = 'broccoli' | 'carrot' | 'lettuce';
+type Fruit =
+  | 'apple'
+  | 'pear'
+  | 'orange';
+
+type Vegetable =
+  | 'broccoli'
+  | 'carrot'
+  | 'lettuce';
 
 // 'apple' | 'pear' | 'orange' | 'broccoli' | 'carrot' | 'lettuce';
 type HealthyFoods = Fruit | Vegetable;
