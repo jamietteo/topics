f4e6acc224296a5455a013f6247ae1622c89d62f
Rastrinak <luisgoncalvesfc@gmail.com>
2021-11-15 16:24:30 +0000

    comentarios

--- mutability.js
+++ mutability.js
@@ -10,20 +10,20 @@ const newUser = (name, location) => {
   };
 };
 
-console.log(User);
+console.log(User); // { name: 'Tiago', location: 'Lagos' }
 
 // allows mutation
 const mutableUser = newUser("Pedro", "Lisboa");
-console.log(mutableUser);
+console.log(mutableUser); // { name: 'Pedro', location: 'Lisboa' }
 
 mutableUser.location = "Lisboa";
 mutableUser.name = "Miguel";
-console.log(mutableUser);
+console.log(mutableUser); // { name: 'Miguel', location: 'Lisboa' }
 
 // doesn't allow mutation
 const immutableUser = Object.freeze(newUser("Luis", "Porto"));
-console.log(immutableUser);
+console.log(immutableUser); // { name: 'Luis', location: 'Porto' }
 
 immutableUser.name = "Jorge";
 immutableUser.location = "Algarve";
-console.log(immutableUser);
+console.log(immutableUser); // { name: 'Luis', location: 'Porto' }
