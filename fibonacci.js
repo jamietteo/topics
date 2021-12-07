4d304094a503e680c91a822089b197f9bff5ab89
Rastrinak <luisgoncalvesfc@gmail.com>
2021-12-07 10:44:35 +0000

    fibonacci last number

--- fibonacci.js
+++ fibonacci.js
@@ -3,4 +3,11 @@ const fibonacci = (n, first = 0, second = 1) =>
     ? []
     : [first, ...fibonacci(n - 1, second, first + second)];
 
-console.log(fibonacci(10));
+console.log(fibonacci(10)); // [0, 1,  1,  2,  3,5, 8, 13, 21, 34]
+
+const fibonacci_lastNumber = (n, first = 0, second = 1) =>
+  (n == 0)
+    ? null
+    : fibonacci_lastNumber(n - 1, second, first + second) || first;
+
+console.log(fibonacci_lastNumber(10)); // 34
