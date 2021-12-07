a0916c84026091962abb63653d55d5b41034be2d
Rastrinak <luisgoncalvesfc@gmail.com>
2021-12-07 10:15:14 +0000

    async

---
+++ async.js
@@ -0,0 +1,15 @@
+const async = (callback) => setTimeout(callback, 100);
+
+const doSomething = () => {
+  const increment = (n, i = 0) => {
+    if (i < n) {
+      console.log(i);
+      async(() => increment(n, i + 1));
+    }
+  }
+
+  async(() => increment(10));
+  async(() => increment(10));
+  async(() => increment(10));
+}
+doSomething();
